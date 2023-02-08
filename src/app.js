3deaf4b14fee11394593ebcc92018636ec256555
Antonio <antonio.garciamn@gmail.com>
2023-02-08 15:21:38 -0300

    layout login and sign-up page

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<HomePage />} />
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/signUp" element={<SignUpPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
