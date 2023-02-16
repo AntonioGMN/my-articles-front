227dd26ae3b5c29457312f0f763df3294eb1270e
Antonio <antonio.garciamn@gmail.com>
2023-02-16 18:59:17 -0300

    fix: usin REACT_APP_BASE_URL

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Base_URL = "http://localhost:5000";
+const Base_URL = process.env.REACT_APP_BASE_URL;
 
 export const instance = axios.create({
 	baseURL: Base_URL,
